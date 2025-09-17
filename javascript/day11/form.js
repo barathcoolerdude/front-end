f4af5f22b3df3dca097c84d6c70dec306e992626
coolerdude <barathelango3@gmail.com>
2025-09-17 17:54:37 +0530

    display user info

--- javascript/day11/form.js
+++ javascript/day11/form.js
@@ -1,9 +1,11 @@
 let form = document.getElementById("myform")
 let msg = document.getElementById("msg")
+let username = document.getElementById("username")
+let email = document.getElementById("email")
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    msg.textContent = `someone selected ${form.city.value}`
+    msg.textContent = `user: ${username.value} with email: ${email.value} from city ${form.city.value}`
 })
 
 
